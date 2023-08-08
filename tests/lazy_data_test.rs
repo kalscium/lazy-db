89b4aa393e4a622870a2e7d8e3fe9fb251483f0e
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-08 21:11:00 +1000

    Added test for writing and reading strings (doesn't pass so far)

--- tests/lazy_data_test.rs
+++ tests/lazy_data_test.rs
@@ -14,3 +14,16 @@ fn lazy_data_new_void() {
     // Type must be void
     assert_eq!(lazy_data.lazy_type, LazyType::Void);
 }
+
+#[test]
+fn lazy_data_string() {
+    let tmp = new_env();
+    let path = tmp.get_path().join("new_string.ld");
+    // Create string file
+    let og_string = String::from("Hello world!");
+    LazyData::new_string(&path, &og_string).unwrap();
+    // Load string file
+    let new_string = LazyData::load(path).unwrap().collect_string().unwrap();
+    // String must be the same
+    assert_eq!(og_string, new_string);
+}
