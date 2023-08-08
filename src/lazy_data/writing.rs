08bf2d4392273ba1c084a9ebbcf90a7d3437f03e
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-08 14:54:05 +1000

    added writing ability for string

--- src/lazy_data/writing.rs
+++ src/lazy_data/writing.rs
@@ -4,4 +4,16 @@ impl LazyData {
     /// Creates a new `LazyData` file with the type of `LazyType::Void`
     #[inline]
     pub fn new_void(path: impl AsRef<Path>) -> Result<(), LDBError> { Self::uninit(path)?; Ok(()) }
+
+    /// Creates a new `LazyData` file with a `String` value and type
+    pub fn new_string(path: impl AsRef<Path>, value: &str) -> Result<(), LDBError> {
+        let mut lazy_data = Self::uninit(path)?;
+        lazy_data.lazy_type = LazyType::String;
+        let ofile = &mut lazy_data.ofile;
+        for i in value.as_bytes() {
+            if let Err(OFileError::IOError(e)) = ofile.write(*i) {
+                return Err(LDBError::IOError(e))
+            };
+        }; Ok(())
+    }
 }
