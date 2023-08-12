e39c9cb2234c6420c9691cdbde315c04dc4210a5
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-12 16:41:34 +1000

    Added a load function to lazy container

--- src/lazy_container.rs
+++ src/lazy_container.rs
@@ -1,11 +1,12 @@
 use crate::*;
 use std::path::{Path, PathBuf};
 use std::fs;
+use std::ffi::OsString;
 
 pub struct LazyContainer {
     path: PathBuf,
-    containers: Vec<PathBuf>,
-    data: Vec<PathBuf>,
+    containers: Vec<OsString>,
+    files: Vec<OsString>,
 }
 
 impl LazyContainer {
@@ -20,7 +21,38 @@ impl LazyContainer {
         Ok(Self {
             path: path.to_path_buf(),
             containers: Vec::new(),
-            data: Vec::new(),
+            files: Vec::new(),
+        })
+    }
+
+    /// Loads a pre-existing `LazyContainer` directory at a specified path.
+    /// 
+    /// Will throw an error if the directory doesn't exist or there is an `io::Error`.
+    pub fn load(path: impl AsRef<Path>) -> Result<Self, LDBError> {
+        let path = path.as_ref().to_path_buf();
+
+        // Checks if path exists or not
+        if !path.is_dir() { return Err(LDBError::DirNotFound(path)) };
+
+        // Values
+        let mut containers = Vec::<OsString>::new();
+        let mut files = Vec::<OsString>::new();
+
+        // Loads files and directories
+        let dir = unwrap_result!(fs::read_dir(&path) => |e| Err(LDBError::IOError(e)));
+        for entry in dir.flatten() {
+            if let Ok(file_type) = entry.file_type() {
+                if file_type.is_dir() {
+                    containers.push(entry.file_name());
+                } else { files.push(entry.file_name()) }
+            }
+        };
+
+        // Constructs self
+        Ok(Self {
+            path,
+            containers,
+            files,
         })
     }
 }
