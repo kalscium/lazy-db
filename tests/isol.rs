59146e7e4331e582c7fde3974635482fb6826dfd
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-13 17:03:04 +1000

    Added another test for nested compilations, and added better random algorithm instead of static counter in isol test

--- tests/isol.rs
+++ tests/isol.rs
@@ -2,18 +2,16 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::fmt::Display;
 use std::fmt;
-
-static mut UID: u8 = 0;
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hasher};
 
 pub fn new_env() -> TmpPath {
     // creates 'test_tmp' folder if it doesn't exist
     let path = Path::new("./test_tmp/");
     if !path.exists() {
         fs::create_dir_all(path).expect("Error: couldn't create directory!");
-    }; unsafe {
-        UID += 1;
-        TmpPath::new(format!("./test_tmp/{UID}"))
-    }
+    }; let rand = RandomState::new().build_hasher().finish();
+    TmpPath::new(format!("./test_tmp/{rand}"))
 }
 
 pub struct TmpPath(PathBuf);
--- tests/lazy_data_test.rs
+++ tests/lazy_data_test.rs
@@ -99,7 +99,28 @@ fn lazy_data_compile() {
     // Read from the database
     let database = LazyDB::load_db(path).unwrap();
     let new_string = database.as_container().unwrap().read_data("data").unwrap().collect_string().unwrap();
-    std::io::stdin().read_line(&mut String::new()).unwrap();
+
+    // Must be equal
+    assert_eq!(og_string, new_string);
+}
+
+#[test]
+fn lazy_data_compile_nested() {
+    let tmp = new_env();
+    let mut path = tmp.get_path().join("new_nested_compiled_database");
+    let og_string = String::from("Hello world!");
+
+    { // Writing to the database and compiling
+        let database = LazyDB::init_db(&path).unwrap();
+        let file = database.as_container().unwrap().new_container("nested").unwrap().data_writer("data").unwrap(); // Get file-wrapper
+        LazyData::new_string(file, &og_string).unwrap(); // Writes string
+        let database = database.compile().unwrap();
+        path = database;
+    }
+
+    // Read from the database
+    let database = LazyDB::load_db(path).unwrap();
+    let new_string = database.as_container().unwrap().read_container("nested").unwrap().read_data("data").unwrap().collect_string().unwrap();
 
     // Must be equal
     assert_eq!(og_string, new_string);
