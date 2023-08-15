aaa3b26696485d62c22c0891ce7d66b98b1815d9
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-15 19:44:04 +1000

    Updated version to appease crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ pub use crate::{
     lazy_trait::*,
 };
 
-pub const VERSION: version::Version = version::Version::new(1, 0, 0);
+pub const VERSION: version::Version = version::Version::new(1, 0, 1);
 
 #[macro_export]
 macro_rules! const_eval {
